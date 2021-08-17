cc3cad2ef68e083f203260fed0d254004d3b6279
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-08-18 01:52:50 +0530

    Add files via upload

---
+++ Graphs/bfs_grid.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int dx[] = {1, 0, -1, 0, 1, 1, -1, -1};
+// int dy[] = {0, 1, 0, -1, -1, 1, -1, 1};
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int n, m;
+vector<vector<int>> vis, a;
+ 
+bool isValid(int x, int y) {
+    if(x < 0 || y < 0 || x >= n || y >= m || vis[x][y]) // Change condition
+        return false;
+    return true;
+}
+
+void bfs(int sx, int sy) {
+    queue<pair<int, int>> q;
+    q.push({sx, sy});
+    vis[sx][sy] = 1;
+    while(q.size()) {
+        auto [cx, cy] = q.front();
+        q.pop();
+        for(int i = 0; i < 4; i++) {
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
+            if(isValid(nx, ny)) {
+                // Reduce Queue as much as possible
+                vis[nx][ny] = 1;
+                q.push({nx, ny});
+            }
+        }
+    }
+    return;
+}
+
+void solve() {
+    cin >> n >> m;
+    vis.resize(n);
+    a.resize(n);
+    for(int i = 0; i < n; i++)
+        vis[i].assign(m, 0), a[i].resize(m, 0);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    bfs(0, 0);
+}
+
+int main() {   
+    ios::sync_with_stdio(0); cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while(T--)
+        solve();
+    return 0;
+}
