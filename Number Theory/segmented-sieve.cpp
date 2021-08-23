026232c6d3585a1f1cb20c01db3421dee47f530e
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-08-24 04:14:06 +0530

    Create segmented-sieve.cpp

---
+++ Number Theory/segmented-sieve.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+// O(R - L + 1) log log R + sqrt(R) log log sqrt(R)
+template <int64_t L, int64_t R>
+struct segmentedSieve {
+	vector<int64_t> primes, _primes;
+	vector<bool> _isPrime, mark;
+	segmentedSieve() {
+		int64_t lim = sqrtl(R);
+		_isPrime.assign(R - L + 1, true);
+		mark.assign(lim + 1, false);
+		for(int64_t i = 2; i <= lim; i++)
+			if(!mark[i]) {
+				_primes.emplace_back(i);
+				for(int64_t j = i * i; j <= lim; j += i) mark[j] = true;
+			}
+		
+		for(int64_t i: _primes)
+			for(int64_t j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+				_isPrime[j - L] = false;
+		if(L == 1) _isPrime[0] = false;
+		int num = L - 1;
+		for(int64_t i: _isPrime)
+			if(num++ && i) primes.push_back(num);
+	}
+	bool isPrime(int64_t num) {	
+		return _isPrime[num - L]; 
+	}
+};
+
+signed main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	segmentedSieve<1000'000'000, 1000'000'009> p;
+
+	return 0;
+}
