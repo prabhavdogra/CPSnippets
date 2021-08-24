38938c182b8251739a7b96a0b34838cdcd32aae2
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-08-24 05:33:21 +0530

    Create etf_precom.cpp

---
+++ Number Theory/etf_precom.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+// O(n log log n)
+template <int SZ = 1'000'000>
+struct etf {
+    vector<int> phi;
+    etf() {
+        phi.resize(SZ + 1);
+        phi[0] = 0;
+        phi[1] = 1;
+        for (int i = 2 ; i <= SZ ; i++)
+            phi[i] = i;
+        for (int i = 2 ; i <= SZ ; i++)
+            if(phi[i] == i)
+                for (int j = i ; j <= SZ ; j += i)
+                    phi[j] -= phi[j] / i;
+    }
+};
+
+signed main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    etf<1'000'000> cal;
+    // cal.phi[n];
+    for(int i = 0; i < 17; i++)
+        cout << cal.phi[i] << " ";
+    return 0;
+}
