5d0d2f920b3ba0025a062ff45ea26cec19e62ee3
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-08-24 15:43:03 +0530

    Create divisors_precom.cpp

---
+++ Number Theory/divisors_precom.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+// O(n log n)
+template <int SZ = 1'000'000>
+struct n_divisors {
+	vector<vector<int>> div;
+	n_divisors() {
+		div.resize(SZ + 1);
+        for(int i = 2; i <= SZ; i++) {
+            int j = i;
+            while(j <= SZ)
+                div[j].push_back(i), j += i;
+        }
+	}
+};
+
+signed main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	n_divisors<100'000> t;
+    for(auto it: t.div[12])
+        cout << it << " ";
+    
+	return 0;
+}
