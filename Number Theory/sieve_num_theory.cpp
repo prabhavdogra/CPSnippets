39f55664eb8bcce8a1b59a703910df1fc2526234
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-08-24 16:14:42 +0530

    Update sieve_num_theory.cpp

--- Number Theory/sieve_num_theory.cpp
+++ Number Theory/sieve_num_theory.cpp
@@ -5,21 +5,21 @@ using namespace std;
 // O(n log log n)
 template <int SZ = 1'000'000>
 struct sieve {
-	vector<bool> isPrime;
-	vector<int> primes, spf;
-	sieve() {
-		spf.resize(SZ + 1);
-		isPrime.assign(SZ + 1, true);
-		isPrime[0] = isPrime[1] = false;
-		for(int i = 2; i <= SZ; i++) {
-			if(isPrime[i])
-				primes.push_back(i), spf[i] = i;
-			for(int j = 0; j < primes.size() && i * primes[j] <= SZ && primes[j] <= spf[i]; j++) {
-				isPrime[i * primes[j]] = false;
-				spf[i * primes[j]] = primes[j];
-			}
-		}
-	}
+    vector<bool> isPrime;
+    vector<int> primes, spf;
+    sieve() {
+        spf.resize(SZ + 1);
+        isPrime.assign(SZ + 1, true);
+        isPrime[0] = isPrime[1] = false;
+        for(int i = 2; i <= SZ; i++) {
+            if(isPrime[i])
+                primes.push_back(i), spf[i] = i;
+            for(int j = 0; j < primes.size() && i * primes[j] <= SZ && primes[j] <= spf[i]; j++) {
+                isPrime[i * primes[j]] = false;
+                spf[i * primes[j]] = primes[j];
+            }
+        }
+    }
     vector<int> primeFactors(int n) {
         vector<int> res;
         while(spf[n]) {
@@ -54,20 +54,11 @@ struct sieve {
     }
 };
 
+sieve<1'000'000> t;
+
 signed main() {
-	ios::sync_with_stdio(0); cin.tie(0);
-	sieve<1'000'000> p;
-    for(auto it: p.divisors(12))
-        cout << it << " ";
-    cout << '\n';
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    for(auto it: p.primeFactors(420))
-        cout << it << " ";
-    cout << '\n';
-
-	for(auto [key, freq]: p.factorize(60))
-        cout << key << " " << freq << '\n';
-    cout << '\n';
-
-	return 0;
+    return 0;
 }
