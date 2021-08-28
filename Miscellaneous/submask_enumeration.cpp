7f173a5bc099b2769d55ba3e16be365242a6115c
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-08-29 02:13:50 +0530

    Create submask_enumeration.cpp

---
+++ Miscellaneous/submask_enumeration.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+// Note: 0 subMask not included
+
+// O(2^setBits)
+void submaskEnumeration(int mask) {
+    for(int subMask = mask; subMask; subMask = (subMask - 1) & mask) {
+        // Use subMask
+    }
+}
+
+// O(3^n)
+void allSubmaskEnumeration(int mask) {
+    int n;
+    for(int mask = 0; mask < (1 << n); mask++) {
+        for(int subMask = mask; subMask; subMask = (subMask - 1) & mask) {
+            // Use subMask
+        }
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    return 0;
+}
