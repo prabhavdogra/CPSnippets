47066b33bc17f3454181eefd8ac60211cd2498c8
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-10-17 13:53:09 +0530

    Update ncr_precom.cpp

--- Number Theory/ncr_precom.cpp
+++ Number Theory/ncr_precom.cpp
@@ -6,7 +6,10 @@ constexpr int64_t M = 1e9 + 7;
 class pnc {
 public:
     int mod_;
-    vector<int> fac, inv;
+    vector<int> fac, finv;
+    int inv(int n, int mod) {
+        return modpow(n, mod - 2, mod);
+    }
     int modpow(int x, int n, int m) {
         if(!x) return 0;
         int res = 1;
@@ -22,17 +25,17 @@ public:
     pnc(int n, int mod) {
         this->mod_ = mod;
         fac.assign(n + 1, 0);
-        inv.assign(n + 1, 0);
+        finv.assign(n + 1, 0);
         fac[0] = 1;
         for(int i = 1; i <= n; i++)
             fac[i] = (fac[i - 1] * i) % mod;
-        inv[n] = modpow(fac[n], mod - 2, mod);
+        finv[n] = inv(fac[n], mod);
         for(int i = n - 1 ; i >= 0; i--)
-            inv[i] = (inv[i + 1] * (i + 1)) % mod;
+            finv[i] = (finv[i + 1] * (i + 1)) % mod;
     }
     int ncr(int n, int r) {
         if(n < r || n < 0 || r < 0) return 0;
-        return fac[n] * inv[r] % mod_ * inv[n - r] % mod_;
+        return fac[n] * finv[r] % mod_ * finv[n - r] % mod_;
     }
     int npr(int n, int r) {
         return ncr(n, r) * fac[r] % mod_;
