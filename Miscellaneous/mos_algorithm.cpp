00ccaca20ddd4da64125440ab9530eb7f404c93b
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-12-12 12:16:26 +0530

    Create mos_algorithm.cpp

---
+++ Miscellaneous/mos_algorithm.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct query {
+    int l, r, id;
+};
+
+struct queryResult { 
+    int cnt = 0; 
+};
+
+struct mos {
+    static void add(vector<int> &a, int id, queryResult &cur);
+    static void remove(vector<int> &a, int id, queryResult &cur);
+    static vector<queryResult> cal(vector<int> &a, vector<query> &q) {
+        int l = 0, r = -1, n = a.size(), blk_sz = sqrt(n);
+        queryResult cnt;
+        vector<queryResult> ans(q.size());
+        sort(q.begin(), q.end(), [&] (const query &p1, const query &p2) {
+            if(p1.l/blk_sz == p2.l/blk_sz) 
+                return ((p1.l/blk_sz) & 1) ? (p1.r > p2.r) : (p1.r < p2.r);
+            return (p1.l/blk_sz < p2.l/blk_sz);
+        });
+        for(int i = 0; i < q.size(); i++) {
+            int ql = q[i].l;
+            int qr = q[i].r;
+            int id = q[i].id;
+            // Extending range
+            while (l > ql) add(a, --l, cnt);
+            while (r < qr) add(a, ++r, cnt);
+            // Reducing range
+            while (l < ql) remove(a, l++, cnt);
+            while (r > qr) remove(a, r--, cnt);
+            ans[id] = cnt;
+        }
+        return ans;
+    }
+};
+
+void mos::add(vector<int> &a, int id, queryResult &cur) {
+    
+}
+void mos::remove(vector<int> &a, int id, queryResult &cur) {
+    
+}
+// mos::cal(a, q);
+
+signed main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    return 0;
+}
