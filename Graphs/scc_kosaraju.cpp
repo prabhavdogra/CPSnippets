cd97f7d03b1e8ba0f41bb0da52211423dfee10d8
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2022-01-09 23:38:56 +0530

    Update scc_kosaraju.cpp

--- Graphs/scc_kosaraju.cpp
+++ Graphs/scc_kosaraju.cpp
@@ -33,7 +33,7 @@ void solve() {
 	auto order = topo_dfs(adj);
 	auto scc = [&] (vector<vector<int>> adj_, vector<int> order_) -> vector<vector<int>> {
 		vector<vector<int>> radj_(adj_.size());
-		for(int i = 0; i < adj_.size(); i++)
+		for(int i = 0; i < (int)adj_.size(); i++)
 			for(auto j: adj[i])
 				radj_[j].push_back(i);
 		vector<vector<int>> components_;
@@ -54,7 +54,7 @@ void solve() {
 		return components_;
 	};
 	auto components = scc(adj, order);
-	auto condense = [&] (vector<vector<int>> adj_, vector<vector<int>> &components_) -> vector<vector<int>> {
+	auto condense = [&] (vector<vector<int>> adj_, vector<vector<int>> &components_) -> pair<vector<int>, vector<vector<int>>> {
 		vector<int> color_(adj_.size());
 		vector<vector<int>> adj_scc_(adj_.size());
 		for(auto &component_: components_) {
@@ -69,10 +69,12 @@ void solve() {
 					adj_scc_[color_[i]].push_back(color_[j]);
 			}
 		}
-		return adj_scc_;
+		vector<int> order_scc_;
+		for(auto component_: components_)
+			order_scc_.push_back(component_[0]);
+		return {order_scc_, adj_scc_};
 	};
-	// components[i][0] -> Topologic Ordering for the Condensed Graph
-	auto adj_scc = condense(adj, components);
+	auto [order_scc, adj_scc] = condense(adj, components);
 }
 
 signed main() {
