495b5a886be6077fa577d7beb882531fcd705e8f
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2022-01-31 01:28:04 +0530

    Update modint.cpp

--- Miscellaneous/modint.cpp
+++ Miscellaneous/modint.cpp
@@ -1,169 +1,77 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-template <std::uint32_t P> struct ModInt32 {
-   public:
-    using i32 = std::int32_t;
-    using u32 = std::uint32_t;
-    using i64 = std::int64_t;
-    using u64 = std::uint64_t;
-    using m32 = ModInt32;
+template<uint32_t M_32>
+class ModInt {
+public:
+	uint64_t data = 0;
+	ModInt() {};
+	ModInt(int64_t val) : data(Modulus(val)){};
+	static uint64_t Modulus(int64_t val) { return ((val % M_32) < 0 ? (val % M_32) + M_32 : val % M_32); }
 
-   private:
-    u32 v;
-    static constexpr u32 get_r() {
-        u32 iv = P;
-        for (u32 i = 0; i != 4; ++i) iv *= 2U - P * iv;
-        return -iv;
-    }
-    static constexpr u32 r = get_r(), r2 = -u64(P) % P;
-    static_assert((P & 1) == 1);
-    static_assert(-r * P == 1);
-    static_assert(P < (1 << 30));
-    static constexpr u32 pow_mod(u32 x, u64 y) {
-        u32 res = 1;
-        for (; y != 0; y >>= 1, x = u64(x) * x % P)
-            if (y & 1) res = u64(res) * x % P;
-        return res;
-    }
-    static constexpr u32 reduce(u64 x) { return x + u64(u32(x) * r) * P >> 32; }
-    static constexpr u32 norm(u32 x) { return x - (P & -(x >= P)); }
-
-   public:
-    static constexpr u32 get_pr() {
-        u32 tmp[32] = {}, cnt = 0;
-        const u64 phi = P - 1;
-        u64 m = phi;
-        for (u64 i = 2; i * i <= m; ++i)
-            if (m % i == 0) {
-                tmp[cnt++] = i;
-                while (m % i == 0) m /= i;
-            }
-        if (m != 1) tmp[cnt++] = m;
-        for (u64 res = 2; res != P; ++res) {
-            bool flag = true;
-            for (u32 i = 0; i != cnt && flag; ++i)
-                flag &= pow_mod(res, phi / tmp[i]) != 1;
-            if (flag) return res;
-        }
-        return 0;
-    }
-    constexpr ModInt32() : v(0){};
-    ~ModInt32() = default;
-    constexpr ModInt32(u32 _v) : v(reduce(u64(_v) * r2)) {}
-    constexpr ModInt32(const m32& rhs) : v(rhs.v) {}
-    constexpr u32 get() const { return norm(reduce(v)); }
-    explicit constexpr operator u32() const { return get(); }
-    explicit constexpr operator i32() const { return i32(get()); }
-    constexpr m32& operator=(const m32& rhs) { return v = rhs.v, *this; }
-    constexpr m32 operator-() const {
-        m32 res;
-        return res.v = (P << 1 & -(v != 0)) - v, res;
-    }
-    constexpr m32 inv() const { return pow(-1); }
-    constexpr m32& operator+=(const m32& rhs) {
-        return v += rhs.v - (P << 1), v += P << 1 & -(v >> 31), *this;
-    }
-    constexpr m32& operator-=(const m32& rhs) {
-        return v -= rhs.v, v += P << 1 & -(v >> 31), *this;
-    }
-    constexpr m32& operator*=(const m32& rhs) {
-        return v = reduce(u64(v) * rhs.v), *this;
-    }
-    constexpr m32& operator/=(const m32& rhs) {
-        return this->operator*=(rhs.inv());
-    }
-    friend m32 operator+(const m32& lhs, const m32& rhs) {
-        return m32(lhs) += rhs;
-    }
-    friend m32 operator-(const m32& lhs, const m32& rhs) {
-        return m32(lhs) -= rhs;
-    }
-    friend m32 operator*(const m32& lhs, const m32& rhs) {
-        return m32(lhs) *= rhs;
-    }
-    friend m32 operator/(const m32& lhs, const m32& rhs) {
-        return m32(lhs) /= rhs;
-    }
-    friend bool operator==(const m32& lhs, const m32& rhs) {
-        return norm(lhs.v) == norm(rhs.v);
-    }
-    friend bool operator!=(const m32& lhs, const m32& rhs) {
-        return norm(lhs.v) != norm(rhs.v);
-    }
-    friend std::istream& operator>>(std::istream& is, m32& rhs) {
-        return is >> rhs.v, rhs.v = reduce(u64(rhs.v) * r2), is;
-    }
-    friend std::ostream& operator<<(std::ostream& os, const m32& rhs) {
-        return os << rhs.get();
-    }
-    constexpr m32 pow(i64 y) const {
-        if ((y %= P - 1) < 0)
-            y += P - 1;  // phi(P) = P - 1, assume P is a prime number
-        m32 res(1), x(*this);
-        for (; y != 0; y >>= 1, x *= x)
-            if (y & 1) res *= x;
-        return res;
-    }
-};
-template <uint32_t Modulus> class Modular {
-    using M = Modular;
-
-   public:
-    static_assert(int(Modulus) >= 1, "Modulus must be in the range [1, 2^31)");
-    static constexpr int modulus() { return Modulus; }
-    static M raw(uint32_t v) { return *reinterpret_cast<M*>(&v); }
-    Modular() : v_(0) {}
-    Modular(int64_t v) : v_((v %= Modulus) < 0 ? v + Modulus : v) {}
-    template <class T>
-    explicit operator T() const {
-        return v_;
-    }
-    M& operator++() { return v_ = ++v_ == Modulus ? 0 : v_, *this; }
-    M& operator--() { return --(v_ ? v_ : v_ = Modulus), *this; }
-    M& operator*=(M o) { return v_ = uint64_t(v_) * o.v_ % Modulus, *this; }
-    M& operator/=(M o) {
-        auto [inv, gcd] = extgcd(o.v_, Modulus);
-        assert(gcd == 1);
-        return *this *= inv;
-    }
-    M& operator+=(M o) {
-        return v_ = int(v_ += o.v_ - Modulus) < 0 ? v_ + Modulus : v_, *this;
-    }
-    M& operator-=(M o) {
-        return v_ = int(v_ -= o.v_) < 0 ? v_ + Modulus : v_, *this;
-    }
-    friend M operator++(M& a, int) { return std::exchange(a, ++M(a)); }
-    friend M operator--(M& a, int) { return std::exchange(a, --M(a)); }
-    friend M operator+(M a) { return a; }
-    friend M operator-(M a) { return a.v_ = a.v_ ? Modulus - a.v_ : 0, a; }
-    friend M operator*(M a, M b) { return a *= b; }
-    friend M operator/(M a, M b) { return a /= b; }
-    friend M operator+(M a, M b) { return a += b; }
-    friend M operator-(M a, M b) { return a -= b; }
-    template <class T>
-    friend T& operator>>(T& is, M& x) {
-        int64_t v;
-        return is >> v, x = v, is;
-    }
-    template <class T>
-    friend T& operator<<(T& os, M x) {
-        return os << x.v_;
-    }
-    friend bool operator==(M a, M b) { return a.v_ == b.v_; }
-    friend bool operator!=(M a, M b) { return a.v_ != b.v_; }
-
-   private:
-    static std::pair<int, int> extgcd(int a, int b) {
-        std::array<int, 2> x{1, 0};
-        while (b) std::swap(x[0] -= a / b * x[1], x[1]), std::swap(a %= b, b);
-        return {x[0], a};
-    }
-    uint32_t v_;
+	ModInt& operator++() { return (++data == M_32) ? 0 : data, *this; }
+	ModInt& operator--() { return --(data ? data : data = M_32), *this; }
+	ModInt operator++(int) { 
+		ModInt res = *this;
+		return (++data == M_32) ? 0 : data, res;
+	}
+	ModInt operator--(int) { 
+		ModInt res = *this;
+		return --(data ? data : data = M_32), res;
+	}
+	friend ostream& operator<<(ostream& os, const ModInt& obj) {
+		return (os << obj.data);
+	}
+	friend istream& operator>>(istream& is, ModInt& obj) {
+		int64_t val;
+		return (is >> val), obj.data = Modulus(val), is;
+	}
+	friend ModInt operator+(const ModInt& a, const ModInt& b) {
+		ModInt res;
+		return res.data = Modulus(a.data + b.data), res;
+	}
+	friend ModInt operator-(const ModInt& a, const ModInt& b) {
+		ModInt res;
+		return res.data = Modulus(a.data - b.data), res;
+	}
+	friend ModInt operator*(const ModInt& a, const ModInt& b) {
+		ModInt res;
+		return res.data = Modulus(Modulus(a.data) * Modulus(b.data)), res;
+	}
+	friend ModInt operator/(const ModInt& a, const ModInt& b) {
+		auto [inv, gcd] = extgcd(b.data, M_32);
+		assert(gcd == 1);
+		return Modulus(a.data * inv);
+	}
+	ModInt& operator+=(ModInt a) { return *this = *this + a, *this; }
+	ModInt& operator-=(ModInt a) { return *this = *this - a, *this; }
+	ModInt& operator*=(ModInt a) { return *this = *this * a, *this; }
+	ModInt& operator/=(ModInt a) { return *this = *this / a, *this; }
+	template<typename T>
+	void operator=(const T& a) { return (*this).data = Modulus(a), void(); }
+	friend bool operator==(ModInt a, ModInt b) { return a.data == b.data; }
+	friend bool operator!=(ModInt a, ModInt b) { return a.data != b.data; }
+	static pair<uint64_t, uint64_t> extgcd(int64_t a, int64_t b) {
+		array<int, 2> x{1, 0};
+		while (b) swap(x[0] -= a / b * x[1], x[1]), swap(a %= b, b);
+		return {x[0], a};
+	}
 };
 
-const int mod_ = 1e9 + 7;
-using mint = ModInt32<mod_>;
+template<typename T, typename S>
+T pow(T x, S n) {
+	T res = 1;
+	while (n) {
+		if (n & 1) res = (res * x);
+		n = (n >> 1);
+		x = (x * x);
+	}
+	return res;
+}
 
-signed main() {
-    mint x;
+const int MOD = 1 ? 1e9 + 7 : 998'244'353;
+using mint = ModInt<MOD>;
+
+int main() {
+	mint a(12);
 }
