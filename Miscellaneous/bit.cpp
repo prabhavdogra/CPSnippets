2076862a221ec51cc63748bdf26213a0c0c6d7a9
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2022-02-13 19:18:00 +0530

    Update bit.cpp

--- Miscellaneous/bit.cpp
+++ Miscellaneous/bit.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 #define int long long int
 
-class bit {
+template<int32_t base>
+class Bit {
 public:
-	int64_t n, base, n_prev;
+	int64_t n, n_prev;
 	vector<int> bits;
 	void update() {
 		if(n == n_prev) return;
@@ -13,9 +14,8 @@ public:
 		int64_t nc = n;
 		while(nc) bits.push_back(nc % base), nc /= base;
 	}
-	bit(int64_t n_, int base_) {
+	Bit(int64_t n_) {
 		n = n_;
-		base = base_;
 		update();
 	}
 	int operator[](int id) {
@@ -24,7 +24,7 @@ public:
 	int64_t count() {
 		return update(), accumulate(bits.begin(), bits.end(), 0ll);
 	}
-	friend ostream& operator<<(ostream& os, bit& obj) {
+	friend ostream& operator<<(ostream& os, Bit& obj) {
 		obj.update();
 		for(int i = (int)obj.bits.size() - 1; i >= 0; i--) 
 			cout << (char)((obj.bits[i] <= 9) ? obj.bits[i] + '0' : (obj.bits[i] - 10 + 'A'));
@@ -32,9 +32,11 @@ public:
 	}
 };
 
+using bit = Bit<16>;
+
 signed main() {
 	cin.tie(nullptr)->sync_with_stdio(false);
-	bit b(676, 16);
+	bit b(676);
 	b.n++;
 	cout << b;
 	return 0;
